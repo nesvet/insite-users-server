78fdaad4304a38eb2de30a131d3924a9bfa9da09
Eugene Nesvetaev <dev@nesvet.dev>
2024-06-02 09:39:17 +0400

    fix: keep process.env.NODE_ENV

--- .conveyer.js
+++ .conveyer.js
@@ -2,21 +2,16 @@ import path from "node:path";
 import { Conveyer, ESBuild } from "@nesvet/conveyer";
 
 
-const { NODE_ENV } = process.env;
-
 const distDir = "dist";
-
 
 const common = {
 	external: true,
 	platform: "node",
 	format: "esm",
 	sourcemap: true,
-	target: "node20",
-	define: {
-		"process.env.NODE_ENV": JSON.stringify(NODE_ENV)
-	}
+	target: "node20"
 };
+
 
 new Conveyer([
 	
