f9cc509b6231fed985296a6b26def943f324da8d
Eugene Nesvetaev <dev@nesvet.dev>
2024-09-03 17:58:03 +0400

    fix: AvatarDoc type needs to be exported

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export type { AbilitiesMap, AbilityBoolean, AbilityParam, AbilityParamItems, Abi
 export type { Org, OrgDoc, Orgs } from "./orgs";
 export type { Role, RoleDoc, Roles } from "./roles";
 export type { Session, SessionDoc, Sessions } from "./sessions";
-export { Users } from "./users";
+export { type AvatarDoc, Users } from "./users";
 export type { Options, User, UserDoc } from "./users";
--- src/users/index.ts
+++ src/users/index.ts
@@ -1,3 +1,4 @@
+export type { AvatarDoc } from "./avatars";
 export type { UserDoc } from "./types";
 export type { User } from "./User";
 export * from "./Users";
